82f02f8559dd5d658bf7398f0992e1882c50354d
Huijing Liang <47288523+MollyLiang@users.noreply.github.com>
2021-09-21 23:13:12 -0700

    Initial

---
+++ MovieShop/ApplicationCore/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ApplicationCore.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Revenue { get; set; }
+        public string PosterUrl { get; set; }
+    }
+}
---
+++ MovieShop/Infrastructure/Services/MovieService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using ApplicationCore.Models;
+using ApplicationCore.ServiceInterfaces;
+using Infrastructure.Repositories;
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        public IEnumerable<MovieCardResponseModel> Get30HighestiGrossingMovies()
+        {
+            var repo = new MovieRepository();
+
+           
+            var movies = repo.Get30HighestGrossingMovies();
+            var moviesCardResponseModel = new List<MovieCardResponseModel>();
+
+            
+            foreach (var movie in movies)
+            {
+                moviesCardResponseModel.Add(new MovieCardResponseModel { Id = movie.Id, PosterUrl = movie.PosterUrl });
+            }
+          
+            return moviesCardResponseModel;
+        }
+    }
+}
